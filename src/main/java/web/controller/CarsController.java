f814425f8fae05ffe99a89979fa0ed988294821d
Evgeny Varlamov <varlamov-el@mail.ru>
2023-01-19 19:00:51 +0300

    Without DAO

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -5,23 +5,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.dao.CarDAO;
 import web.model.Car;
+import web.service.CarServiceImpl;
 
 import java.util.List;
 
 @Controller
 public class CarsController {
-    private final CarDAO carDAO;
+    private final CarServiceImpl carService;
 
     @Autowired
-    public CarsController(CarDAO carDAO) {
-        this.carDAO = carDAO;
+    public CarsController(CarServiceImpl carDAO) {
+        this.carService = carDAO;
     }
 
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(value = "count", defaultValue = "5") int count, ModelMap modelMap) {
-        List<Car> cars = carDAO.getCars(count);
+        List<Car> cars = carService.getCars(count);
         modelMap.addAttribute("cars", cars);
         return "cars";
     }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,23 +1,28 @@
 package web.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import web.dao.CarDAO;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CarServiceImpl implements CarService {
-    private final CarDAO carDAO;
+    private static long CAR_ID;
+    private List<Car> carList;
 
-    @Autowired
-    public CarServiceImpl(CarDAO carDAO) {
-        this.carDAO = carDAO;
+    {
+        carList = new ArrayList<>();
+        carList.add(new Car(++CAR_ID, "Tucson NX4 Diesel", 2.0));
+        carList.add(new Car(++CAR_ID, "RAV4 4WD", 2.5));
+        carList.add(new Car(++CAR_ID, "Tiguan Turbo", 1.4));
+        carList.add(new Car(++CAR_ID, "CX5", 2.0));
+        carList.add(new Car(++CAR_ID, "Outlander", 2.5));
     }
 
     @Override
     public List<Car> getCars(int count) {
-        return carDAO.getCars(count);
+        return count >= carList.size() ? carList : carList.stream().limit(count).collect(Collectors.toList());
     }
 }
